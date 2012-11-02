0b9f8bdc9962835e528902a5b170f19297f4efd3
Jonas Wagner <jwagner@raptus.com>
2012-11-02 06:22:06 +0100

    Moved to github

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.0b2'
 setup(name='pcommerce.shipment.pickup',
       version=version,
       description="Provides a pick up shipment plugin for PCommerce",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
@@ -16,7 +16,7 @@ setup(name='pcommerce.shipment.pickup',
       keywords='',
       author='Raptus AG',
       author_email='dev@raptus.com',
-      url='https://svn.plone.org/svn/collective/pcommerce.shipment.pickup',
+      url='https://github.com/Raptus/pcommerce.shipment.pickup',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['pcommerce', 'pcommerce.shipment'],
